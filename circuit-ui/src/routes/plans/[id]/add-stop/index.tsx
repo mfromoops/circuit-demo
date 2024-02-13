6ec4b7aaf36ec6839a95ef7f199cb4ae1374dcc9
Marito <mariofloresdev@gmail.com>
2024-02-13 00:06:39 -0400

    added users and coordinates inputs

--- circuit-ui/src/routes/plans/[id]/add-stop/index.tsx
+++ circuit-ui/src/routes/plans/[id]/add-stop/index.tsx
@@ -8,7 +8,8 @@ import {
 } from "@builder.io/qwik-city";
 import { twMerge } from "tailwind-merge";
 import { CircuitAPI } from "~/business-logic/utils";
-import { Card, CardHeading } from "~/components/ui/UIComponents";
+import { TextField } from "~/components/ui/Fields";
+import { Button, Card, CardHeading } from "~/components/ui/UIComponents";
 import { useUserList } from "~/routes/layout";
 
 export const usePlan = routeLoader$(async ({ env, url }) => {
@@ -52,16 +53,19 @@ export const useAddStop = routeAction$(async (body, { env, url }) => {
 });
 
 type UsePlanType = ReturnType<typeof usePlan>;
+type UserUsersListType = ReturnType<typeof useUserList>;
 
 export default component$(() => {
   const plan = usePlan();
-  const userList = useUserList();
+  const users = useUserList();
   const activeTab: Signal<"search" | "user" | "coordinates"> =
-    useSignal("search");
+    useSignal("coordinates");
   return (
     <>
       <Tabs acitveTab={activeTab}></Tabs>
-      <SearchForm plan={plan} />
+      {activeTab.value === "search" && <SearchForm plan={plan}></SearchForm>}
+      {activeTab.value === "user" && <Users users={users}></Users>}
+      {activeTab.value === "coordinates" && <Coordinates></Coordinates>}
     </>
   ) 
 });
@@ -179,3 +183,51 @@ export const Tabs = component$(
     );
   },
 );
+
+export const Users = component$((props: {users: UserUsersListType}) => {
+  return (
+        <ul
+          class={
+            "flex flex-col list-none flex-wrap rounded-xl p-1 mt-5 mx-5 gap-2"
+          }
+          data-tabs="tabs"
+          role="list"
+        >
+          {props.users.value.map((user) => (
+            <li
+              class="z-30 flex-auto select-none text-center bg-blue-gray-50/60"
+              onClick$={() => {
+
+              }}
+            >
+              <a
+                href="#search"
+                class="z-30 mb-0 flex flex-col w-full cursor-pointer items-center justify-center rounded-lg border-0 bg-inherit px-0 py-1 text-slate-700"
+                data-tab-target=""
+                role="tab"
+                aria-selected="true"
+              >
+                <div class="ml-1">{user.name}</div>
+                <div class="ml-1">{user.email}</div>
+                <div class="ml-1">{user.addressLineOne}</div>
+                <div class="ml-1">{user.addressLineTwo}</div>
+              </a>
+            </li>
+          ))}
+        </ul>
+  );
+});
+
+export const Coordinates = component$(() => {
+  return (
+    <div class="mt-5">
+      <div class="mx-5 p-5 shadow-md">
+        <Form class="grid gap-2">
+          <TextField label="Latitude" inputName="latitude" />
+          <TextField label="Longitude" inputName="longitude" />
+          <Button type="submit" class="bg-green-500">Search</Button>
+        </Form>
+      </div>
+    </div>
+  );
+});
