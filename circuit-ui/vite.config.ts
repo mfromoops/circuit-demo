3c93f271428edace39dcfb3cb827ad7e419ba6bf
Mario Flores <m.floresrivera@orbisops.com>
2024-06-08 15:33:46 -0400

    allow crypto library

--- circuit-ui/vite.config.ts
+++ circuit-ui/vite.config.ts
@@ -6,6 +6,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig((): UserConfig => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    ssr: {
+      external: ["crypto"]
+    },
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
