cb0629e7d65e492922e0e04097ea7d6c8d4728d2
Mario Flores <m.floresrivera@orbisops.com>
2024-06-08 14:26:48 -0400

    test signature

--- circuit-ui/src/routes/webhooks/index.ts
+++ circuit-ui/src/routes/webhooks/index.ts
@@ -1,6 +1,20 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
-
-export const onPost: RequestHandler = async ({ json, request }) => {
+import crypto, { sign } from "crypto";
+export const onPost: RequestHandler = async ({ json, request, env }) => {
    console.log(JSON.stringify(request.headers));  
+   const signature = request.headers.get("circuit-signature");
+   if(!signature){
+      json(401, { message: "Signature not found" });
+      return;
+   }
+   const secret = env.get("CICRCUIT_SECRET_KEY") as string;
+   const message = await request.text();
+   console.log({ message });
+   const expectedSignature = crypto
+    .createHmac('sha256', secret)
+    .update(message)
+    .digest('hex')
+  console.log({ expectedSignature, signature });
+
   json(200, { message: "Webhook received" });
 };
